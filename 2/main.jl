1727726d4685e5a59a32cab7d37234ef4874a373
Élysa <101974839+ElysaSrc@users.noreply.github.com>
2022-12-02 18:47:28 +0100

    Refactored

--- 2/main.jl
+++ 2/main.jl
@@ -1,73 +1,14 @@
-function to_points(o::Char, s::Char)
-  if s == 'X' # Rock
-    base = 1
-    if o == 'A' # Rock
-      return base + 3
-    elseif o == 'B' # Paper
-      return base + 0
-    elseif o == 'C' # Cisor
-      return base + 6
-    end
-  elseif s == 'Y' # Paper
-    base = 2
-    if o == 'A' # Rock
-      return base + 6
-    elseif o == 'B' # Paper
-      return base + 3
-    elseif o == 'C' # Cisor
-      return base + 0
-    end
-  elseif s == 'Z' # Cisor
-    base = 3
-    if o == 'A' # Rock
-      return base + 0
-    elseif o == 'B' # Paper
-      return base + 6
-    elseif o == 'C' # Cisor
-      return base + 3
-    end
-  end
-end
+points = Dict(
+  "A X" => 4, "B X" => 1, "C X" => 7,
+  "A Y" => 8, "B Y" => 5, "C Y" => 2,
+  "A Z" => 3, "B Z" => 9, "C Z" => 6
+)
+strat = Dict(
+  "A X" => "A Z", "B X" => "B X", "C X" => "C Y",
+  "A Y" => "A X", "B Y" => "B Y", "C Y" => "C Z",
+  "A Z" => "A Y", "B Z" => "B Z", "C Z" => "C X"
+)
 
-function to_strat(o::Char, s::Char)
-  if s == 'X' # lose
-    if o == 'A' # Rock
-      return 'Z'
-    elseif o == 'B' # Paper
-      return 'X'
-    elseif o == 'C' # Cisor
-      return 'Y'
-    end
-
-  elseif s == 'Y' # draw
-    if o == 'A' # Rock
-      return 'X'
-    elseif o == 'B' # Paper
-      return 'Y'
-    elseif o == 'C' # Cisor
-      return 'Z'
-    end
-
-  elseif s == 'Z' # win
-    if o == 'A' # Rock
-      return 'Y'
-    elseif o == 'B' # Paper
-      return 'Z'
-    elseif o == 'C' # Cisor
-      return 'X'
-    end
-  end
-end
-
-s = sum(map(
-  # (x) -> to_points(x[1], x[3]),
-  (x) -> to_points(x[1], to_strat(x[1], x[3])),
-  eachsplit(
-    open("input.txt") do file
-        read(file, String)
-    end,
-    "\n"
-  )
-))
-
-println(s)
+data = eachsplit(read(open("input.txt"), String), "\n")
+println(sum((x) -> points[x], data))
+println(sum((x) -> points[strat[x]], data))
